6b47739dc82ba6c839c6046aa1c434f012dc6404
Grace Cornel <202380082@psu.palawan.edu.ph>
2025-09-18 22:22:33 +0800

    mb, kanina yung allauth, ngayon urls hehe

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -5,6 +5,7 @@ from studentorg import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('', HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
